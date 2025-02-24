a129142f94bb8bb3d48430a3e811cf7b0a83b634
mtume <mtume2016@gmail.com>
2025-02-24 23:53:44 +0300

    deployment test 2

--- src/api/addcomments.ts
+++ src/api/addcomments.ts
@@ -1,15 +1,2 @@
 
-import { drizzle } from 'drizzle-orm/neon-http';
-import { eq } from 'drizzle-orm';
-import { usersTable } from './db/schema';
-import { commentsTable } from '../db/schema';
-
-
-const db = drizzle(import.meta.env.VITE_DATABASE_URL)
-
-
-async function addComment(){
-    const comment: typeof commentsTable.$inferInsert = {
-        comment: 
-    }
-}
+//emptiness
